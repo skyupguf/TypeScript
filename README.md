d04e40221b43852950d0c87150e420f735af049c
skyupguf <skyupguf@gmail.com>
2021-06-26 17:41:30 +0900

    backup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # TypeScript
 practice TypeScript log
 2021-06-26
-1. TypeScript 개발환경 구축하기
+- TypeScript 개발환경 구축: npm, node, typescript, prettier, eslint 설정
---
+++ compile/test.js
@@ -0,0 +1,4 @@
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = { firstName: "Max", lastName: "Pane" };
